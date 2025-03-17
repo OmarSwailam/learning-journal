f6454ce7e4d4ad10fbe6fe1b348caf83e26ce7d1
Omar Swailam <omarswailam@outlook.com>
2025-03-17 20:59:39 +0200

    feat: article details

--- js/articles.js
+++ js/articles.js
@@ -50,3 +50,41 @@ export const articlesArray = [
     }
 ];
 
+export const articleBody = `
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam repellat perferendis dolorum, 
+    quas adipisci voluptate distinctio est incidunt corporis illum officiis velit doloribus ad laborum necessitatibus. 
+    Sed recusandae minima assumenda vel earum reiciendis sunt atque maiores, quae adipisci. Voluptate sint nisi quod! Vitae, 
+    laboriosam. Praesentium nostrum minus recusandae iure maiores eaque impedit enim, unde beatae ut, 
+    eum est quisquam voluptatem porro, totam facere! Fuga soluta saepe harum nisi facere autem. Repellat, 
+    commodi voluptas, laudantium tempora ad neque harum expedita ex ipsa eos ea recusandae quibusdam, 
+    ipsum consequuntur hic delectus sequi. Voluptate non rem, ea esse cupiditate ad assumenda accusantium laboriosam, 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi beatae, dolorum, 
+    quas nemo similique sequi, eum doloremque vel aut quasi minima laboriosam a maxime natus deleniti eligendi ab.
+    Laborum minima incidunt, at odit ex illo facilis deleniti maiores ducimus id doloribus molestiae adipisci. Nulla, 
+    recusandae expedita amet ut tempora illo quaerat eius, aperiam veniam, cumque vero illum commodi? 
+    Dolores libero tempore, eos officia quas repudiandae mollitia, sunt laborum quisquam quibusdam dolor 
+    pariatur consequatur facilis a iure, rerum consectetur accusamus porro suscipit aliquid hic? Error 
+    quaerat ex ab laborum provident voluptatum nobis? Ratione voluptatem pariatur laboriosam libero, et 
+    ipsum aut aliquid neque quaerat, beatae nisi deserunt optio excepturi in accusamus corrupti qui, blanditiis 
+    laudantium iure? Explicabo aliquid ratione eos similique voluptate obcaecati possimus expedita officia 
+    voluptatum in impedit rem distinctio sit, dolor rerum deserunt non corrupti. Quis voluptas ipsa debitis 
+    ea voluptatibus odit ratione assumenda repellendus, quasi, eius doloremque blanditiis saepe, dignissimos 
+    repellat! Laboriosam provident laborum soluta eaque voluptatem aut odit magni eum officiis perferendis quos esse, 
+    blanditiis doloribus reprehenderit quam dolores autem hic doloremque saepe ipsam nam deleniti amet! Veritatis iste, 
+    quisquam velit facilis est repudiandae accusamus placeat porro veniam voluptatem incidunt dicta temporibus obcaecati! 
+    Inventore saepe animi hic nesciunt alias voluptate aut minus ex. Blanditiis ipsa repellendus eveniet quo, 
+    dolore adipisci earum facilis. Voluptatem totam veritatis ea corporis incidunt? Nostrum animi laborum, 
+    quidem deserunt consequatur ut dolorum repellat quas ratione fugit repudiandae, quaerat quis possimus, 
+    maiores provident. Illum obcaecati voluptatibus rerum laudantium nostrum, unde recusandae est quos quas 
+    atque deserunt repellendus fugiat voluptatem! Vero, adipisci sequi reiciendis quo id animi perferendis 
+    recusandae labore iure odit deleniti! Ullam at earum quis amet omnis veniam praesentium ad sapiente 
+    voluptas quidem labore soluta blanditiis, ratione consequuntur ea sit ut minima. Nobis officia earum 
+    explicabo iste quae, nihil, sequi ad provident vero perspiciatis culpa ut nam minus beatae accusamus 
+    magnam quam. Autem quidem aut minima ducimus nulla perferendis perspiciatis qui aliquam aperiam. Maxime 
+    minus accusamus iusto quo quas vero possimus veniam ratione fuga nesciunt neque commodi ullam atque ex nam,
+    veritatis laborum similique, distinctio molestiae illum necessitatibus alias, quidem in? Consequuntur 
+    quisquam quod quam ab. Dicta omnis, aliquid soluta sequi, itaque fuga nostrum quisquam nam natus ducimus, 
+    aspernatur velit asperiores ut corrupti. Eius, officia accusantium aliquid architecto explicabo assumenda excepturi 
+    laudantium, ex expedita numquam laboriosam suscipit, voluptate non ab! Voluptas ratione quam temporibus repellendus eius 
+    consequuntur. Cupiditate doloribus ducimus ullam necessitatibus, ipsum non.
+`
--- js/index.js
+++ js/index.js
@@ -1,53 +1,115 @@
-import { articlesArray } from "./articles.js";
+import { articlesArray, articleBody } from "./articles.js";
+
+const featuredArticle = articlesArray[0]
+const articles = articlesArray.slice(1, 4);
+const moreArticles = articlesArray.slice(4);
+
+function getRecentArticles(id) {
+    const filteredArticles = articlesArray.filter(article => article.id !== id);
+    const shuffledArticles = filteredArticles.sort(() => Math.random() - 0.5);
+    return shuffledArticles.slice(0, 3);
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger")
     const navItems = document.getElementById("nav-items")
-    hamburger.addEventListener("click", (e) => {
+    const home = document.getElementById("home");
+    const logo = document.getElementById("logo");
+    const mainContainer = document.getElementById("main")
+
+    function toggleHamburger() {
         console.log("clicked")
         navItems.classList.toggle("expand");
-    })
+    }
 
-    const featuredArticle = articlesArray[0]
-    const articles = articlesArray.slice(1, 4);
-    const moreArticles = articlesArray.slice(4);
+    function renderHome() {
+        mainContainer.innerHTML = `
+            <div class="featured" id="featured"></div>  
+            <div class="articles" id="articles"></div>
+        `
+        const featuredContainer = document.getElementById("featured")
+        const articlesContainer = document.getElementById("articles")
 
 
-    const featuredContainer = document.getElementById("featured")
-    featuredContainer.innerHTML = `
-        <p class="created-at">${featuredArticle.createdAt}</p>
-        <h1 class="title">${featuredArticle.title}</h1>
-        <p class="brief">${featuredArticle.brief}</p>
-    `
+        featuredContainer.innerHTML = `
+            <p class="created-at">${featuredArticle.createdAt}</p>
+            <a class="title" data-id="${featuredArticle.id}" >${featuredArticle.title}</a>
+            <p class="brief">${featuredArticle.brief}</p>
+        `
 
-    const articlesContainer = document.getElementById("articles")
-    articlesContainer.innerHTML = articles.map((article) => {
-        return `
+        articlesContainer.innerHTML = articles.map((article) => {
+            return `
             <div class="article">
                 <img class="article-img" src=${article.imgUrl}>
                 <p class="created-at">${article.createdAt}</p>
-                <h1 class="title">${article.title}</h1>
+                <a data-id="${article.id}" class="title">${article.title}</a>
                 <p class="brief">${article.brief}</p>
             </div>
         `
-    }).join("") + `
+        }).join("") + `
         <button class="view-more" id=view-more>View More</button>
     `
 
-    const viewMoreBtn = document.getElementById("view-more")
-    viewMoreBtn.addEventListener("click", (e) => {
-        viewMoreBtn.style.display = 'none';
-        articlesContainer.innerHTML += moreArticles.map((article) => {
-            return `
-                <div class="article">
-                    <img class="article-img" src=${article.imgUrl}>
-                    <p class="created-at">${article.createdAt}</p>
-                    <h1 class="title">${article.title}</h1>
-                    <p class="brief">${article.brief}</p>
-                </div>
-            
-            `
-        }).join("")
-    })
+        const viewMoreBtn = document.getElementById("view-more")
+        if (viewMoreBtn) {
+            viewMoreBtn.addEventListener("click", (e) => {
+                viewMoreBtn.style.display = 'none';
+                articlesContainer.innerHTML += moreArticles.map((article) => {
+                    return `
+                    <div class="article">
+                        <img class="article-img" src=${article.imgUrl}>
+                        <p class="created-at">${article.createdAt}</p>
+                        <a data-id="${article.id}" class="title">${article.title}</a>
+                        <p class="brief">${article.brief}</p>
+                    </div>
+    
+                `
+                }).join("")
+            })
+        }
 
+        const titles = document.querySelectorAll(".title")
+        titles.forEach((title) => {
+            title.addEventListener("click", (e) => {
+                const articleId = parseInt(e.target.dataset.id);
+                renderArticle(articlesArray.filter((article) => article.id === articleId)[0])
+            })
+        })
+    }
+
+    function renderArticle(article) {
+        const bodyLength = articleBody.length;
+        const splitIndex = Math.floor(bodyLength / 2);
+        const firstPart = articleBody.slice(0, splitIndex);
+        const secondPart = articleBody.slice(splitIndex);
+
+        mainContainer.innerHTML = `
+        <div class="article-display">
+            <button class="back-btn" id=back-btn> &larr; back </button>
+            <p class="created-at">${article.createdAt}</p>
+            <h1 class="title">${article.title}</h1>
+            <p class="brief">${article.brief}</p>
+            <img class="article-img" src=${article.imgUrl}>
+            <h3 class="article-sub-label">Introduction</h3>
+            <p class="article-body">${firstPart}</p>
+
+            <h3 class="article-sub-label">More Details</h3>
+            <p class="article-body">${secondPart}</p>
+        </div>
+
+    `
+
+        const backBtn = document.getElementById("back-btn")
+        backBtn.addEventListener("click", renderHome)
+    }
+
+    function renderRecentArticles() {
+
+    }
+
+    hamburger.addEventListener("click", toggleHamburger)
+    home.addEventListener("click", renderHome);
+    logo.addEventListener("click", renderHome);
+
+    renderHome()
 });
