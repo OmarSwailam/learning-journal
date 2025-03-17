135bc9ef141fb9ec9cf1606e4651ea952483e8c7
Omar Swailam <omarswailam@outlook.com>
2025-03-17 18:57:18 +0200

    feat: view more btn

--- js/index.js
+++ js/index.js
@@ -9,7 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     const featuredArticle = articlesArray[0]
-    const articles = articlesArray.slice(1);
+    const articles = articlesArray.slice(1, 4);
+    const moreArticles = articlesArray.slice(4);
 
 
     const featuredContainer = document.getElementById("featured")
@@ -29,5 +30,24 @@ document.addEventListener("DOMContentLoaded", () => {
                 <p class="brief">${article.brief}</p>
             </div>
         `
-    }).join("")
+    }).join("") + `
+        <button class="view-more" id=view-more>View More</button>
+    `
+
+    const viewMoreBtn = document.getElementById("view-more")
+    viewMoreBtn.addEventListener("click", (e) => {
+        viewMoreBtn.style.display = 'none';
+        articlesContainer.innerHTML += moreArticles.map((article) => {
+            return `
+                <div class="article">
+                    <img class="article-img" src=${article.imgUrl}>
+                    <p class="created-at">${article.createdAt}</p>
+                    <h1 class="title">${article.title}</h1>
+                    <p class="brief">${article.brief}</p>
+                </div>
+            
+            `
+        }).join("")
+    })
+
 });
